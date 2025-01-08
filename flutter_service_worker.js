d62c5b38b2860808b5e95a4e15d9ccb9a790614e
anotherlusitano <108989500+anotherlusitano@users.noreply.github.com>
2025-01-08 09:06:21 +0000

    Deploying to gh-pages from @ anotherlusitano/anotherlusitano.github.io@3608f223919cd4e7fef18ae31f5a5cc087408f01 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "c167c30c1801ec2f7d59eb9c7a592425",
-"flutter_bootstrap.js": "6d7c164fd7cca74ef1e3892cca8364d6"};
+"flutter_bootstrap.js": "cd7f51d1b6abb3f28192bae20f111898"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
