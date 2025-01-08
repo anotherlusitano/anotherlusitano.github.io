09d12f88312bdf764678eaa209fb7aea80532228
anotherlusitano <108989500+anotherlusitano@users.noreply.github.com>
2025-01-08 08:56:45 +0000

    Deploying to gh-pages from @ anotherlusitano/anotherlusitano.github.io@a5795c5c982a69a636533360ef2ca7a333902287 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "dc8c6480319473e2475c1c84bb08d033",
 "assets/AssetManifest.bin.json": "92616a0464a75de839ce747b76c8ef1e",
-"index.html": "9d2d2dddd20b71544ba1549da38b9f3c",
-"/": "9d2d2dddd20b71544ba1549da38b9f3c",
+"index.html": "7bc36b128f33424cbe436ada26a38a82",
+"/": "7bc36b128f33424cbe436ada26a38a82",
 "manifest.json": "67c18ec67723a8453d5f773572661f16",
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
@@ -46,7 +46,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "c167c30c1801ec2f7d59eb9c7a592425",
-"flutter_bootstrap.js": "6e2bff544025cb93e56ccdb80eb4152a"};
+"flutter_bootstrap.js": "6d7c164fd7cca74ef1e3892cca8364d6"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
