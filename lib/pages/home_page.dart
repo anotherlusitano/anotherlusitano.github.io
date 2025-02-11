306c234c9b58ab18fde9d50b65fd396a7eac7354
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-02-11 18:12:58 +0000

    Refactor Home Page
    This removes the views and wraps the PrimaryButtons with the Wrap
    widget.
    
    The old layout didn't use the Wrap widget, so it needed to have
    different widgets like Column and Row to align the buttons without
    getting the overflow

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
-import 'package:anotherlusitano_github_io/config/layout.dart';
-import 'package:anotherlusitano_github_io/views/mobile/home_view.dart';
 import 'package:flutter/material.dart';
 
-import '../views/desktop/home_view.dart';
+import '../../components/grid_background.dart';
+import '../../components/primary_button.dart';
+import '../../config/assets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,13 +10,78 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(builder: (context, constraints) {
-        if (constraints.maxWidth > Layout.mobileWidth) {
-          return DesktopHomeView();
-        } else {
-          return MobileHomeView();
-        }
-      }),
+      body: Stack(
+        children: [
+          GridBackground(),
+
+          // Content
+          SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 40),
+                  Container(
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(color: Colors.black, shape: BoxShape.circle),
+                    child: ClipOval(
+                      child: SizedBox.fromSize(
+                        size: Size.fromRadius(180),
+                        child: Image.asset(
+                          Assets.itsme,
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Welcome, I'm João",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  SelectableText.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(text: "A computer science student driven by a \n"),
+                        TextSpan(text: "passion for simplicity and quality."),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  SizedBox(height: 40),
+                  Wrap(
+                    alignment: WrapAlignment.center,
+                    spacing: 25.0,
+                    runSpacing: 25.0,
+                    children: [
+                      PrimaryButton(
+                        url: "mailto:joao.rosado.ribeiro@gmail.com",
+                        icon: Assets.email,
+                        text: "Email me",
+                      ),
+                      PrimaryButton(
+                        url: "https://www.linkedin.com/in/joaoribeiro0/",
+                        icon: Assets.linkedin,
+                        text: "Linkedin",
+                      ),
+                      PrimaryButton(
+                        url: "https://github.com/anotherlusitano",
+                        icon: Assets.github,
+                        text: "Github",
+                      ),
+                      PrimaryButton(
+                        url: "https://ko-fi.com/anotherlusitano",
+                        icon: Assets.kofi,
+                        text: "Support me",
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
