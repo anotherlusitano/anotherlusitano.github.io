a4ef67c6d0322a69451183c534f465fbf31ff2fb
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-02-18 15:13:15 +0000

    Remove usePathUrlStrategy
    For some reason, if we use this function we can't navigate the routes
    using the url directly

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:anotherlusitano_github_io/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  usePathUrlStrategy();
   runApp(const App());
 }
